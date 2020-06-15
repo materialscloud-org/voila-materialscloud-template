9f54a6b8c7fc14f91aff80c729d7cc7358f57bc9
Dou <dou.du.epfl@gmail.com>
2020-06-15 17:45:17 +0200

    Use the cookiecutter template

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+import os
+
+data_files = []
+for root, dirs, files in os.walk('share'):
+    root_files = [os.path.join(root, i) for i in files]
+    data_files.append((root, root_files))
+
+setup_args = {
+    'name': 'voila-materialscloud-template',
+    'version': '0.0.1',
+    'packages': [],
+    'data_files': data_files,
+    'install_requires': [
+        'voila>=0.1.6,<0.2'
+    ],
+    'author': 'Dou Du',
+    'url': 'https://github.com/materialscloud-org/materialscloud-voila-template'
+}
+
+if __name__ == '__main__':
+    setup(**setup_args)
