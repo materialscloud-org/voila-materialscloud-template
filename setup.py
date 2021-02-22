3acce220c4959b6453f51fa08c15d61a5aeb41cd
Casper Welzel Andersen <casper.andersen@epfl.ch>
2021-02-22 11:52:54 +0100

    Move setup.py requirements to txt files

--- setup.py
+++ setup.py
@@ -5,11 +5,10 @@ from setuptools import setup
 from setuptools.command.develop import develop
 from setuptools.command.install import install
 
-
 def post_install():
     """Copy materialscloud templates' files to Jupyter config location"""
     for template in ("materialscloud", "materialscloud-iframe"):
-        check_call(['./copy_voila_template.py', template], cwd=Path(__file__).parent.resolve())
+        check_call(['./copy_voila_template.py', template], cwd=TOP_DIR)
 
 class ExtraInstall(install):
     """Post-installation for installation (install) mode."""
@@ -29,6 +28,18 @@ class ExtraDevelop(develop):
         post_install()
 
 
+TOP_DIR = Path(__file__).parent.resolve()
+
+with open(TOP_DIR.joinpath("README.md")) as handle:
+    README = handle.read()
+
+with open(TOP_DIR.joinpath("requirements_base.txt")) as handle:
+    REQUIREMENTS_BASE = [f"{_.strip()}" for _ in handle.readlines() if " " not in _]
+
+with open(TOP_DIR.joinpath("requirements_dev.txt")) as handle:
+    REQUIREMENTS_DEV = [f"{_.strip()}" for _ in handle.readlines() if " " not in _]
+
+
 setup(
     name='voila-materialscloud-template',
     version='0.3.3',
@@ -38,15 +49,13 @@ setup(
     url='https://github.com/materialscloud-org/voila-materialscloud-template',
     license='BSD',
     description='Voilà template for Materials Cloud that implements the Materials Cloud header and CSS.',
-    long_description=open(Path(__file__).parent.joinpath('README.md').resolve()).read(),
+    long_description=README,
     long_description_content_type='text/markdown',
     keywords='voila jupyter materialscloud',
     cmdclass={
         'develop': ExtraDevelop,
         'install': ExtraInstall,
     },
-    install_requires=['voila~=0.2.6'],
-    extras_require={
-        'dev': open(Path(__file__).parent.joinpath('requirements.txt').resolve()).readlines() + ['pylint']
-    }
+    install_requires=REQUIREMENTS_BASE,
+    extras_require={'dev': REQUIREMENTS_DEV}
 )
